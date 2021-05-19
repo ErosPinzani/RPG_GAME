f881f34cac8d9a4619cc4da2693ba5ed79ab99e6
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2021-05-19 17:58:01 +0200

    Movement test

--- Tests/Collisions.h
+++ Tests/Collisions.h
@@ -1,145 +1,69 @@
 //
-// Created by erosp on 11/02/2021.
+// Created by erosp on 14/05/2021.
 //
 
-#ifndef ENEMY_CPP_COLLISIONS_H
-#define ENEMY_CPP_COLLISIONS_H
+#ifndef AOEBULLET_CPP_COLLISIONS_H
+#define AOEBULLET_CPP_COLLISIONS_H
 
 #include "gtest/gtest.h"
+#include <windows.h>
+
 #include "../Hero.h"
 #include "../Enemy.h"
-#include "../Map.h"
-#include "../Chest.h"
-#include "../AoeBullet.h"
 #include "../Function.h"
-
+/*
 class Collisions: public testing::Test {
 public:
-    AoeBullet Bullet1;
-    Enemy Enemy1;
-    TextDisplayClass TextDisplay1;
-    vector<sptr<AoeBullet>> AoeBulletArray;
+    //Enemy Enemy1;
+    TextDisplayClass TextDisplay;
     vector<sptr<Enemy>> enemyArray;
-    vector<sptr<Chest>> chestArray;
     vector<sptr<TextDisplayClass>> textDisplayArray;
+    sf::Clock clock;
     std::shared_ptr<Function> function;
 };
 
-TEST_F(Collisions, WallCollision){
-    Map::getInstance()->vectorM1 = std::vector<int> {
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,1,1,1,1,0,1,0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,1,1,1,1,
-    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,
-    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,0,1,1,0,0,0,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,
-    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,1,1,1,0,1,1,0,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+TEST_F(Collisions, Enemy_Hero) {
+    Map::getInstance()->vectorM1 = std::vector<int>{
+            1,1,1,
+            1,0,1,
+            1,0,1,
+            1,1,1
     };
+    Hero Hero(10,10,3,50,50);
+    std::vector<int>({50,85});
+    //Enemy Enemy(10,10,1,2,50,85);
+    //test vita iniziale eroe
+    Function::enemy_collision_player(Hero, enemyArray, TextDisplay, textDisplayArray, clock);
+    ASSERT_EQ(Hero.hp,10);
+    //test vita finale eroe
+    Hero.UpdateMovement(true, sf::Keyboard::S);
+    Function::enemy_collision_player(Hero, enemyArray, TextDisplay, textDisplayArray, clock);
+    ASSERT_EQ(Hero.hp,8);
 
-    Bullet1.rect.setPosition(100, 100);
-
-    Bullet1.direction = Bullet::left;
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100 - Bullet1.getMovementSpeed());
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    Bullet1.direction = Bullet::right;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    Bullet1.direction = Bullet::down;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100 + Bullet1.getMovementSpeed());
-
-    Bullet1.direction = Bullet::up;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    for (int i = 0; i < 100; i++)
-        Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.destroy, true);
+}
+*/
 
 
-
-    Map::getInstance()->vectorM1 = std::vector<int> {
-            1,1,1,1,1,1,1,1,1,1,1,1,
-            1,0,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,0,1,
-            1,0,0,0,0,0,0,0,0,0,0,1,
-            1,1,1,1,1,1,1,1,1,1,1,1
+/*
+TEST(Collision, Enemy_Hero) {
+    Map::getInstance()->vectorM1 = std::vector<int>{
+            1,1,1,
+            1,0,1,
+            1,0,1,
+            1,1,1
     };
-    Bullet1.rect.setPosition(100, 100);
-    Bullet1.destroy = false;
+    Hero Hero1(10,10,3,50,50);
+    Enemy Enemy1(10,10,1,2,50,85);
+    vector<sptr<Enemy>> enemyArray;
+    TextDisplayClass TextDisplay;
+    vector<sptr<TextDisplayClass>> textDisplayArray;
+    sf::Clock clock;
 
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    Bullet1.direction = Bullet::left;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100 - Bullet1.getMovementSpeed());
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    Bullet1.direction = Bullet::right;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    Bullet1.direction = Bullet::down;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100 + Bullet1.getMovementSpeed());
-
-    Bullet1.direction = Bullet::up;
-    Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ((int)Bullet1.rect.getPosition().y, 100);
-
-    for (int i = 0; i < 100; i++)
-        Bullet1.Update();
-    ASSERT_EQ((int)Bullet1.destroy, true);
+    ASSERT_EQ(Hero1.hp,10);
+    Hero1.UpdateMovement(true, sf::Keyboard::S);
+    Function::enemy_collision_player(Hero1, enemyArray, TextDisplay, textDisplayArray, clock);
+    ASSERT_EQ(Hero1.hp,8);
 }
 
-TEST_F(Collisions, EnemyCollision){
-    Enemy1.rect.setPosition(50, 100);
-    ASSERT_EQ(Enemy1.rect.getPosition().x, 50);
-    ASSERT_EQ(Enemy1.rect.getPosition().y, 100);
-
-    Bullet1.rect.setPosition(100, 100);
-    ASSERT_EQ(Bullet1.rect.getPosition().x, 100);
-    ASSERT_EQ(Bullet1.rect.getPosition().y, 100);
-
-    ASSERT_EQ(Enemy1.hp, 15);
-    ASSERT_EQ(Bullet1.getAttackDamage(), 2);
-
-    Bullet1.direction = Bullet::left;
-    for (int i = 0; i < 10; i++) {
-        std::cout<<Enemy1.hp<<endl;
-        Bullet1.Update();
-        function->aoe_collision(AoeBulletArray, enemyArray, chestArray, TextDisplay1, textDisplayArray);
-    }
-
-    ASSERT_EQ((int)Bullet1.destroy, true);
-    ASSERT_EQ(Enemy1.hp, Enemy1.hp - Bullet1.getAttackDamage());
-}
-
-#endif //ENEMY_CPP_COLLISIONS_H
+*/
+#endif //AOEBULLET_CPP_COLLISIONS_H
--- Tests/Movement.h
+++ Tests/Movement.h
@@ -7,6 +7,7 @@
 
 #include "gtest/gtest.h"
 #include "../Hero.h"
+#include "../Enemy.h"
 #include "../Map.h"
 #include <windows.h>
 
@@ -43,6 +44,42 @@ TEST(Character, Movement) {
     Hero.UpdateMovement(true, sf::Keyboard::S);
     ASSERT_EQ((int)Hero.rect.getPosition().x, 53);
     ASSERT_EQ((int)Hero.rect.getPosition().y, 53);
+}
+
+
+TEST(Enemy, Movement) {
+    Map::getInstance()->vectorM1 = std::vector<int>{
+            1,1,1,
+            1,0,1,
+            1,0,1,
+            1,1,1
+    };
+    Enemy Enemy(10, 10, 3, 5, 50, 50);
+
+    //test posizione iniziale
+    ASSERT_EQ((int)Enemy.rect.getPosition().x, 50);
+    ASSERT_EQ((int)Enemy.rect.getPosition().y, 50);
+
+    //test movimento e collisione muro
+    ASSERT_EQ(Enemy.Walkable(Enemy.left), false);
+    Enemy.UpdateMovement(true, Character::left);
+    ASSERT_EQ((int)Enemy.rect.getPosition().x, 50);
+    ASSERT_EQ((int)Enemy.rect.getPosition().y, 50);
+
+    ASSERT_EQ(Enemy.Walkable(Enemy.right), true);
+    Enemy.UpdateMovement(true, Character::right);
+    ASSERT_EQ((int)Enemy.rect.getPosition().x, 53);
+    ASSERT_EQ((int)Enemy.rect.getPosition().y, 50);
+
+    ASSERT_EQ(Enemy.Walkable(Enemy.up), false);
+    Enemy.UpdateMovement(true, Character::up);
+    ASSERT_EQ((int)Enemy.rect.getPosition().x, 53);
+    ASSERT_EQ((int)Enemy.rect.getPosition().y, 50);
+
+    ASSERT_EQ(Enemy.Walkable(Enemy.down), true);
+    Enemy.UpdateMovement(true, Character::down);
+    ASSERT_EQ((int)Enemy.rect.getPosition().x, 53);
+    ASSERT_EQ((int)Enemy.rect.getPosition().y, 53);
 }
 
 #endif //MAIN_CPP_MOVEMENT_H
